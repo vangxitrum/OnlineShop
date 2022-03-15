5bc9705bd4dcc63de74965ef8bb56546fc11a67e
19522541 <19522541@gm.uit.edu.vn>
2022-03-15 19:26:39 +0700

    _product_detail
    trang thong tin san pham

--- src/api/controllers/home-page-controller.js
+++ src/api/controllers/home-page-controller.js
@@ -2,7 +2,6 @@ const { render } = require('node-sass')
 const ProductDetail = require('../models/productDetail')
 const Photo = require('../models/photo')
 const { mongooseToObject } = require('../../../util/mongoose')
-const photo = require('../models/photo')
 class HomePageController {
   show(req, res, next) {
     // var dbo = ProductDetail.db('onlineshop');
--- src/api/controllers/product-detail-controller.js
+++ src/api/controllers/product-detail-controller.js
@@ -1,8 +1,25 @@
+const { query } = require('express');
 const { render } = require('node-sass')
-
+const Photo = require('../models/photo')
+var mongoose = require('mongoose');
+const ProductDetail = require('../models/productDetail')
+const ProductQuantity = require('../models/productQuantity')
 class ProductDetailController{
     show(req,res,next){
-        res.render('pages/ShopPage/product-detail.ejs',{auth:false,PageIndex: 1});
+       const productID= req.query.id
+       console.log("productID:"+productID)
+       ProductDetail.find({_id: productID})
+       .then((product) => {
+        Photo.find({ productDetailID: productID })
+        .then((photos) => {
+            ProductQuantity.find({ productDetailID: productID })
+            .then((quantity)=>{
+              console.log(`solong la :${quantity.length}`)
+              res.render('pages/ShopPage/product-detail.ejs',{auth:false,PageIndex: 1,product,photos,quantity});
+            })   
+         });
+       })
+     
     }
 
 
